44e5eb5e5a62123066fe59baa29af2e4e62e9332
Dimaetor <53823825+Coskntkk@users.noreply.github.com>
2021-08-07 18:06:11 +0300

    construction continued
    -login page is almost completed
    -functionality added
    -readme updated with screenshot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # old-twitter-clone
  Clone of the old Twitter, 2007-2008. Reference is images on the internet.
+
+<img src="readme_images\loginPage.png">
--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,31 @@
+const headerWhat = "What";
+const headerWhy = "Why";
+const headerHow = "How";
+
+const paragraphWhat = "Twitter is a service for friends, family, anc co-workers to communicate and stay connected through the exchange of quick, frequent answers to one simple questions: <strong>What are you doing?</strong>";
+const paragraphWhy = "Twitter is easy to use and it is completely free. Communicating with your friends, family and co-workers has never been this easy. Most of all, the most important thing is; Twitter is <strong>completely safe.</strong>";
+const paragraphHow = "All you have to do is create an account and find whoever you want! You can connect, communicate and get know about them with Twitter. Click the button below and get started! It is <strong>really easy.</strong>";
+
 $(".wwh-button").click(function(){
+  const selected = this.textContent;
+
   $(".wwh-button").removeClass("wwh-enabled");
   $(".wwh-button").addClass("wwh-disabled");
 
-  $(this).removeClass("wwh.disabled");
+  $(this).removeClass("wwh-disabled");
   $(this).addClass("wwh-enabled");
 
-  console.log(this.textContent);
+  if ( selected == "How?" ) {
+    header = headerHow;
+    paragraph = paragraphHow;
+  } else if ( selected == "Why?" ) {
+    header = headerWhy;
+    paragraph = paragraphWhy;
+  } else if ( selected == "What?" ) {
+    header = headerWhat;
+    paragraph = paragraphWhat;
+  }
+
+  $(".header-wwh").text(header);
+  $(".twitter-paragraph").html(paragraph);
 })
