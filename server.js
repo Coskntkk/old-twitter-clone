71d721eac84bd3a44bf310d184580b6ed71db0a8
Dimaetor <53823825+Coskntkk@users.noreply.github.com>
2021-08-12 02:27:30 +0300

    Account Page Improved
    Construction is almost completed
    Functionality and form-control builded

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
 
 app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function ( req, res ){
   res.sendFile(__dirname + "/login.html");
@@ -9,7 +13,35 @@ app.get("/", function ( req, res ){
 
 app.get("/account", function ( req, res ){
   res.sendFile(__dirname + "/account.html");
-})
+});
+
+app.post("/account", function(req, res) {
+  const userName = req.body.userName;
+  const userPassword = req.body.userPassword;
+  const userEmail = req.body.userEmail;
+  const checkbox = req.body.userScoop;
+  var subscription = false;
+
+  if (checkbox == "true") {
+    subscription = true;
+  } else {
+    subscription = false;
+  }
+
+  const data = {
+    members: [
+      {
+        name: userName,
+        password: userPassword,
+        email: userEmail,
+        subscribed: subscription,
+      }
+    ]
+  };
+
+  console.log(data);
+  res.sendFile(__dirname + "/login.html");
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Old Twitter Server is on at Port: 3000");
