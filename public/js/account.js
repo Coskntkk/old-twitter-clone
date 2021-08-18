91306473b924143405f8449f107d5d1739b09cdd
Dimaetor <53823825+Coskntkk@users.noreply.github.com>
2021-08-18 23:33:36 +0300

    post methods added for forms

--- public/js/account.js
+++ public/js/account.js
@@ -1,1 +1,9 @@
-
+$('#signupUserName').on('input', function() {
+  var content = $('#signupUserName').val();
+  if(content.includes(" ")){
+    $("#yourUrl").css( "color", "red" );
+  } else {
+    $("#yourUrl").css( "color", "black" );
+  }
+  $("#yourUrl").text(content);
+});
--- server.js
+++ server.js
@@ -2,47 +2,88 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const https = require("https");
+const ejs = require('ejs');
 
 app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function ( req, res ){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/login.html");
 });
 
-app.get("/account", function ( req, res ){
+app.post("/", function(req, res) {
+  var loginID = req.body.loginName;
+  var loginPw = req.body.loginPassword;
+  var rememberCheck = req.body.loginRemember;
+  var remember = false;
+  if (rememberCheck == "true") {
+    remember = true;
+  } else {
+    remember = false;
+  }
+
+  var admin = {
+    name: "coskntkk",
+    password: "supersecret",
+    email: "coskntkk@gmail.com"
+  };
+
+
+  var data = {
+    login: [{
+      name: loginID,
+      password: loginPw,
+      rememberLogin: remember
+    }]
+  };
+  console.log(data);
+
+  if (loginID == admin["name"] && loginPw == admin["password"]) {
+    res.sendFile(__dirname + "/home.html");
+  } else {
+    res.sendFile((__dirname + "/login.html"));
+  };
+
+});
+
+app.get("/account", function(req, res) {
   res.sendFile(__dirname + "/account.html");
 });
 
 app.post("/account", function(req, res) {
-  const userName = req.body.userName;
-  const userPassword = req.body.userPassword;
-  const userEmail = req.body.userEmail;
-  const checkbox = req.body.userScoop;
+  var userName = req.body.userName;
+  var userPassword = req.body.userPassword;
+  var userEmail = req.body.userEmail;
+  var checkbox = req.body.userScoop;
   var subscription = false;
-
   if (checkbox == "true") {
     subscription = true;
   } else {
     subscription = false;
   }
 
-  const data = {
-    members: [
-      {
-        name: userName,
-        password: userPassword,
-        email: userEmail,
-        subscribed: subscription,
-      }
-    ]
+  var success;
+
+  if ((userName.includes(" ")) || (userPassword.includes(" ") || userPassword.length < 6)) {
+    success = false;
+  }
+
+  var data = {
+    members: [{
+      name: userName,
+      password: userPassword,
+      email: userEmail,
+      subscribed: subscription,
+    }]
   };
 
   console.log(data);
   res.sendFile(__dirname + "/login.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Old Twitter Server is on at Port: 3000");
 });
